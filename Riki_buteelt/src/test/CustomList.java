754b8d4e495c412f73dd64d21ca3ea738379ae18
Gan-Erdene <Gan-Erdene@DESKTOP-H5DJKDG>
2024-02-06 18:09:51 +0800

    6,09pm

--- Riki_buteelt/src/test/CustomList.java
+++ Riki_buteelt/src/test/CustomList.java
@@ -60,66 +60,66 @@ public class CustomList<T> {
         int choice;
 
         do {
-            System.out.println("Menu:");
-            System.out.println("1. Add a book");
-            System.out.println("2. Remove a book");
-            System.out.println("3. Print all books");
-            System.out.println("4. Check if a book exists");
-            System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
+            System.out.println("Цэс:");
+            System.out.println("1. Ном нэмэх");
+            System.out.println("2. Ном устгах");
+            System.out.println("3. Бүх номыг харах");
+            System.out.println("4. Ном байгаа эсэхийг шалгах");
+            System.out.println("5. Нарах");
+            System.out.print("Сонголт оо оруулна уу: ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.print("Enter the name of the book: ");
-                    scanner.nextLine(); // Consume newline character
+                    System.out.print("Номын нэр оруулна уу: ");
+                    scanner.nextLine();
                     String name = scanner.nextLine();
-                    System.out.print("Enter the author of the book: ");
+                    System.out.print("Зохиолчийн нэр оруулна уу: ");
                     String author = scanner.nextLine();
                     Book newBook = new Book(name, author);
                     bookList.add(newBook);
-                    System.out.println("Book added successfully.");
+                    System.out.println("Ном амжилттай нэмэгдлээ");
                     break;
                 case 2:
                     if (bookList.size() == 0) {
-                        System.out.println("The list is empty.");
+                        System.out.println("Хоосон байна.");
                         break;
                     }
-                    System.out.print("Enter the index of the book to remove: ");
+                    System.out.print("Номын индэксийг оруулна уу: ");
                     int index = scanner.nextInt();
                     if (index >= 0 && index < bookList.size()) {
                         bookList.remove(index);
-                        System.out.println("Book removed successfully.");
+                        System.out.println("Ном амжилттай устлаа.");
                     } else {
-                        System.out.println("Invalid index.");
+                        System.out.println("Буруу индекс");
                     }
                     break;
                 case 3:
-                    System.out.println("Books in the list:");
+                    System.out.println("Номын жагсаалт:");
                     Book book ;
                     for (int i = 0; i < bookList.size(); i++) {
                     	book = bookList.get(i);
-                        System.out.println((i + 1) + ". Name:" + book.getName() +" Author:" +book.getAuthor());
+                        System.out.println((i + 1) + ". Нэр:" + book.getName() +" Зохиолч:" +book.getAuthor());
                     }
                     break;
                 case 4:
-                    System.out.print("Enter the name of the book to search: ");
+                    System.out.print("Хайх номын нэр: ");
                     scanner.nextLine(); // Consume newline character
                     String searchName = scanner.nextLine();
-                    System.out.print("Enter the author of the book to search: ");
+                    System.out.print("Хайх номын зохиолч: ");
                     String searchAuthor = scanner.nextLine();
                     Book searchBook = new Book(searchName, searchAuthor);
                     if (bookList.contains(searchBook)) {
-                        System.out.println("The book exists in the list.");
+                        System.out.println("Ном олдлоо.");
                     } else {
-                        System.out.println("The book does not exist in the list.");
+                        System.out.println("Ном олдсонгүй");
                     }
                     break;
                 case 5:
-                    System.out.println("Exiting...");
+                    System.out.println("Гарч байна...");
                     break;
                 default:
-                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+                    System.out.println("Буруу сонголт 1-5 хооронд сонгоно уу");
             }
         } while (choice != 5);
 
